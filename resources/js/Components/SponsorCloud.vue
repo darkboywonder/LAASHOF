3f84f0523e928a774102d6f05639319f0a01396a
Shawn Jones <shawn.greg.jones@gmail.com>
2023-01-16 20:38:47 -0600

    Update padding

--- resources/js/Components/SponsorCloud.vue
+++ resources/js/Components/SponsorCloud.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white">
-        <div class="mx-auto max-w-7xl py-8 px-6 lg:px-8">
+        <div class="mx-auto max-w-7xl pt-8 pb-16 px-6 lg:px-8">
             <div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-6">
                 <div
                     class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1"
