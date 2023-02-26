a35d9a60a62da7fb8163a8592f68839a70e261eb
Shawn Jones <shawn.greg.jones@gmail.com>
2023-02-26 14:55:24 -0600

    Update nova gate

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -41,7 +41,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return $user->type === 'administrator';
+            return $user->type === 'admin';
         });
     }
 
