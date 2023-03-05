fcc7923cfb34f7867a66b941c02c03eb8537367f
Shawn Jones <shawn.greg.jones@gmail.com>
2023-03-05 14:53:04 -0600

    add scope, doesntExist helper, and update relationship

--- app/Models/Inductee.php
+++ app/Models/Inductee.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Inductee extends Model
 {
@@ -21,8 +23,26 @@ class Inductee extends Model
         return $this->belongsTo(Nominator::class);
     }
 
+    public function nominatable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
     public function relative()
     {
-        return $this->hasOne(Relative::class);
+        return $this->hasOne(Relative::class, 'nominee_id');
     }
-}
+
+    public function scopeExist(Builder $query, Nominee $nominee): void {
+        $query->where([
+            'first_name' => $nominee->first_name,
+            'last_name' => $nominee->last_name,
+            ])->where(function (Builder $query) use ($nominee) {
+                $query->where('birthday', $nominee->birthday)->orWhereNull('birthday');
+            });
+        }
+
+    public static function doesntExist(Nominee $nominee) {
+        return (bool) ! self::exist($nominee)->first();
+    }
+    }
